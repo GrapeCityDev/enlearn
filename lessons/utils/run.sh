c4e3f1d94efeef04f1cad2e04a0d73995fd253a5
AlexYang <alex.yang@grapecity.com>
2014-03-11 14:35:32 -0700

    Add testing data

---
+++ lessons/utils/run.sh
@@ -0,0 +1,7 @@
+
+for i in {3..900}
+do
+   filename=`printf %03d $i`
+   CP  -R US_6.2_TG_Unit2_RP6 US_6.2_TG_Unit2_RP6-$filename
+   mv US_6.2_TG_Unit2_RP6-$filename/US_6.2_TG_Unit2_RP6.html US_6.2_TG_Unit2_RP6-$filename/US_6.2_TG_Unit2_RP6-$filename.html
+done
